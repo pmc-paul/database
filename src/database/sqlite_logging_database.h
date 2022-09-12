684020c48dda5f31ecebfe9cdfb5fbcfc1e495f0
Victoria <victoria.pitz.clairoux@usherbrooke.ca>
2022-09-12 14:01:29 -0400

    requests to db works with basic subscriber

--- src/database/sqlite_logging_database.h
+++ src/database/sqlite_logging_database.h
@@ -1,37 +1,10 @@
-#pragma once
+// #pragma once
 
-// #include "database/logging_database.h"
-#include <sqlite3.h>
+// // #include "database/logging_database.h"
+// #include <sqlite3.h>
+// #include <stdio.h>
 
-namespace database
-{
-    class SQLiteLoggingDatabase //: public LoggingDatabase
-    {
-        public:
+// namespace database
+// {
 
-            SQLiteLoggingDatabase(std::string sqlite_database_file);
-            ~SQLiteLoggingDatabase();
-
-            bool logEvent(Event event);
-            bool logError(Error error);
-            bool logTiming(Timing timing);
-            bool logMetric(Metric metric);
-            Event loadEvent(std::string uuid);
-            Error loadError(std::string uuid);
-            Timing loadTiming(std::string uuid);
-            Metric loadMetric(std::string uuid);
-
-        private:
-            sqlite3* db_connection;
-            void checkError(int error_code);
-    };
-
-    class SQLiteError : public std::runtime_error
-    {
-        public:
-            explicit SQLiteError(std::string what) : std::runtime_error(what)
-            {
-
-            }
-    };
-}
+// }
--- src/sqlite_logging_database.cpp
+++ src/sqlite_logging_database.cpp
@@ -1,97 +1,53 @@
 #include "database/sqlite_logging_database.h"
 #include <ros/ros.h>
+#include <sqlite3.h>
+#include <stdio.h>
+#include "std_msgs/String.h"
 
-namespace database
-{
-    SQLiteLoggingDatabase::SQLiteLoggingDatabase(std::string sqlite_database_file)
-    {
-        int err = 0;
-        err = sqlite3_open(sqlite_database_file.c_str(), &(this->db_connection));
-        if(err)
-        {
-            ROS_ERROR_STREAM("Error opening sqlite database file: " << sqlite_database_file << " : " << sqlite3_errmsg(this->db_connection));            
-        }
-    }
-
-    SQLiteLoggingDatabase::~SQLiteLoggingDatabase()
-    {
-        sqlite3_close(this->db_connection);
-    }
-
-    bool SQLiteLoggingDatabase::logEvent(Event event)
-    {
-        sqlite3_stmt * stmt;
-        std::string sql = "INSERT INTO events (uuid,  type, parent_event_uuid, timestamp) VALUES (?, ?, ?, ?)";
-
-        try
-        {
-            int err = sqlite3_prepare_v2(this->db_connection, sql.c_str(), -1, &stmt, NULL);
-            this->checkError(err);
-            err = sqlite3_bind_text(stmt, 1, event.uuid.c_str(), event.uuid.length(), SQLITE_TRANSIENT);
-            this->checkError(err);
-            err = sqlite3_bind_text(stmt, 2, event.type.c_str(), event.type.length(), SQLITE_TRANSIENT);
-            this->checkError(err);
-            err = sqlite3_bind_text(stmt, 3, event.parent_uuid.c_str(), event.parent_uuid.length(), SQLITE_TRANSIENT);
-            this->checkError(err);
-            err = sqlite3_bind_int64(stmt, 4, event.timestamp.toNSec());
-            this->checkError(err);
-            err = sqlite3_step(stmt);
-            this->checkError(err);
-            err = sqlite3_finalize(stmt);
-            this->checkError(err);
-        }
-        catch(const SQLiteError e)
-        {
-            ROS_ERROR_STREAM(e.what() << '\n');
-        }
-    }
-
-    bool SQLiteLoggingDatabase::logError(Error error)
-    {
-
-    }
-
-    bool SQLiteLoggingDatabase::logTiming(Timing timing)
-    {
-
-    }
-
-    bool SQLiteLoggingDatabase::logMetric(Metric metric)
-    {
-
-    }
-
-    Event SQLiteLoggingDatabase::loadEvent(std::string uuid)
-    {
-
-    }
-
-    Error SQLiteLoggingDatabase::loadError(std::string uuid)
-    {
-
-    }
-
-    Timing SQLiteLoggingDatabase::loadTiming(std::string uuid)
-    {
-
-    }
-
-    Metric SQLiteLoggingDatabase::loadMetric(std::string uuid)
-    {
-
-    }
-
-    void SQLiteLoggingDatabase::checkError(int error_code)
-    {
-        if(error_code)
-        {
-            std::string err_msg = sqlite3_errmsg(this->db_connection);
-            ROS_ERROR_STREAM("SQLITE ERROR: " << err_msg);
-            throw SQLiteError(err_msg);
-        }
-    }
-
+static int callback(void *data, int argc, char **argv, char **azColName){
+   int i;
+   fprintf(stderr, "%s: ", (const char*)data);
+   
+   for(i = 0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   
+   printf("\n");
+   return 0;
 }
-c++
-sqlite
-cmake
+
+void sqlCallback(const std_msgs::String msg){
+
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   const char *sql;
+   char const *data = "Callback function called";
+
+   rc = sqlite3_open("/home/victoria/paul/dev_ws/src/database/database/PAUL.db", &db);
+   
+   std::string query_string = "select * from items where name is " + msg.data;
+   sql = query_string.c_str();
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+   if( rc != SQLITE_OK ) {
+   fprintf(stderr, "SQL error: %s\n", zErrMsg);
+   sqlite3_free(zErrMsg);
+   } else {
+   fprintf(stdout, "Operation done successfully\n");
+   }
+   // sqlite3_close(db);
+}
+
+
+int main(int argc, char* argv[]) {
+
+ros::init(argc, argv, "listener");
+
+ros::NodeHandle n;
+ros::Subscriber sub = n.subscribe("sqlRequest", 10, sqlCallback);
+
+ros::spin();
+}
