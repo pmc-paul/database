88692841f334b84205450c68393559625c3d5ead
Victoria <victoria.pitz.clairoux@usherbrooke.ca>
2022-11-13 16:57:11 -0500

    added id in service + changes in db (level)

--- src/sqlReqServer.cpp
+++ src/sqlReqServer.cpp
@@ -4,18 +4,21 @@
 #include <stdio.h>
 #include "std_msgs/String.h"
 #include "database/itemDetails.h"
+#include "database/itemDetailsName.h"
 
 int sql_response;
 std::string image_name;
 float level;
 float width;
 float height;
+std::string id;
 
 static int callback(void *data, int argc, char **argv, char **azColName){
-   image_name = argv[1];
+   image_name = argv[3];
    level = (float)strtod(argv[4],NULL);
    width = (float)strtod(argv[5],NULL);
    height = (float)strtod(argv[6],NULL);
+   id = argv[0];
    return 0;
 }
 
@@ -26,6 +29,7 @@ bool sqlSrv(database::itemDetails::Request  &req, database::itemDetails::Respons
    level = 0.0;
    width = 0.0;
    height = 0.0;
+   id = "";
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
@@ -48,9 +52,43 @@ bool sqlSrv(database::itemDetails::Request  &req, database::itemDetails::Respons
    res.level = level;
    res.width = width;
    res.height = height;
+   res.id = id;
    return true;
 }
 
+bool sqlSrvName(database::itemDetailsName::Request  &req, database::itemDetailsName::Response &res)
+{
+   //reset global variables
+   image_name = "";
+   level = 0.0;
+   width = 0.0;
+   height = 0.0;
+   id = "";
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   const char *sql;
+   char const *data = "Callback function called";
+
+   rc = sqlite3_open("/home/victoria/paul/dev_ws/src/database/database/PAUL.db", &db);
+   
+   std::string query_string = "SELECT * FROM items WHERE image IS '" + req.name + "'";
+   sql = query_string.c_str();
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+   // close database
+   sqlite3_close(db);
+
+   //set response
+   res.image_name = image_name;
+   res.level = level;
+   res.width = width;
+   res.height = height;
+   res.id = id;
+   return true;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -58,6 +96,7 @@ ros::init(argc, argv, "sql_server");
 
 ros::NodeHandle n;
 ros::ServiceServer service = n.advertiseService("sqlRequest", sqlSrv);
+ros::ServiceServer serviceName = n.advertiseService("sqlRequestName", sqlSrvName);
 
 ros::spin();
 }
